731217294c9cacabfdbc08ddbde1ec6ad924e341
Ivan <stazhor@bk.ru>
2022-01-31 20:33:38 +0300

    fix exercise_2.py

--- lesson_3/exercise_2.py
+++ lesson_3/exercise_2.py
@@ -8,6 +8,12 @@ import os.path
 import hashlib
 import json
 
+
+def input_pswd():
+    pswd = input()
+    return hashlib.sha256(salt.encode() + pswd.encode()).hexdigest()  # Преобразуем пароль с солью в хеш.
+
+
 if not os.path.exists('salt.json'):  # Условие для создания файла хранения соли.
     salt = uuid4().hex
     with open('salt.json', 'w', encoding='utf-8') as f:
@@ -21,8 +27,8 @@ with open('salt.json', 'r', encoding='utf-8') as f:  # Считываем сол
     salt = json.load(f)
     print('В базе данных хранится строка: ', salt)
 
-pswd = input('Введите пароль: ')
-hash_pswd = hashlib.sha256(salt.encode() + pswd.encode()).hexdigest()  # Преобразуем пароль с солью в хеш.
+print('Введите пароль: ')
+hash_pswd = input_pswd()
 
 with open('pswd.json', 'r', encoding='utf-8') as f:  # Добавляем получившийся хеш в json файл со списком хешей.
     hash_lst = json.load(f)
@@ -31,9 +37,8 @@ if hash_pswd not in hash_lst:
 with open('pswd.json', 'w', encoding='utf-8') as f:
     json.dump(hash_lst, f)
 
-pswd_check = input('Введите пароль еще раз для проверки: ')
-hash_pswd_check = hashlib.sha256(salt.encode() +
-                                 pswd_check.encode()).hexdigest()  # Преобразуем повторный пароль с солью в хеш.
+print('Введите пароль еще раз для проверки: ')
+hash_pswd_check = input_pswd()
 
 if hash_pswd_check == hash_pswd:  # Проверка на совпадение паролей.
     print('Вы ввели правильный пароль')
